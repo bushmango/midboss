88826b5423dcf46bf3a66bfde57fbaa51eeab087
Unknown <bushmango@gmail.com>
2019-03-08 19:11:24 -0600

    attempt fix

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
-import * as React from 'react'
+//import * as React from 'react'
+
+const React = require('react')
+
 const { useState, useEffect } = React
 import * as PubSub from 'pubsub-js'
 import { _ } from './imports/lodash'
