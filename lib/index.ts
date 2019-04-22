fd5844f5b5460ca810777791311736c96a0251f2
sbushman <steve@stevebushman.com>
2019-04-22 11:06:14 -0500

    simplify

--- lib/index.ts
+++ lib/index.ts
@@ -17,13 +17,10 @@ function log(...x) {
 
 export interface IMidbossOptions<T> {
   useVerbose: boolean
-  useFreeze: boolean
-  useClone: boolean
   useLocalStorage: boolean
   localStorageFields?: Array<keyof T>
   onSave?: (t: T) => void
   onRestore?: (t: T) => T
-  useImmer: boolean
 }
 export interface IMidbossLiveOptions {
   useVerbose: boolean
@@ -41,40 +38,12 @@ export interface IMidboss<T> {
   rehydrate: (changes: Partial<T>) => any
 }
 
-// See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze
-function deepFreeze(object) {
-  if (!Object.freeze) {
-    return object
-  }
-  // Retrieve the property names defined on object
-  let propNames = Object.getOwnPropertyNames(object)
-
-  // Freeze properties before freezing self
-  for (let name of propNames) {
-    let value = object[name]
-    if (value) {
-      let type = typeof value
-      if (type === 'object' || type === 'function') {
-        object[name] = deepFreeze(value)
-      }
-    }
-  }
-  return Object.freeze(object)
-}
-
-function tryCloneAndFreeze<T>(
+function onStateChanged<T>(
   state,
   saver: flavorSaver.IFlavorSaver<T>,
   options: IMidbossOptions<T>
 ) {
   let nextState = state
-
-  if (options.useClone) {
-    nextState = _.cloneDeep(state)
-  }
-  if (options.useFreeze) {
-    deepFreeze(nextState)
-  }
 
   if (options.useLocalStorage && saver) {
     saver.save(nextState)
@@ -86,13 +55,6 @@ function tryCloneAndFreeze<T>(
   return nextState
 }
 
-function tryFreeze<T>(state, options: IMidbossOptions<T>) {
-  if (options.useFreeze) {
-    state = deepFreeze(state)
-  }
-  return state
-}
-
 export function createMidboss<T>(
   stateKey,
   version: string,
@@ -100,10 +62,7 @@ export function createMidboss<T>(
   options: Partial<IMidbossOptions<T>>
 ): IMidboss<T> {
   let _options: IMidbossOptions<T> = _.defaults(options, {
-    useFreeze: true,
-    useClone: false,
     useLocalStorage: false,
-    useImmer: true,
     localStorageFields: null,
   })
 
@@ -133,23 +92,17 @@ export function createMidboss<T>(
     initialState = _options.onRestore(initialState)
   }
 
-  let state = tryCloneAndFreeze(initialState, saver, _options)
-
-  if (_options.useImmer) {
-    state = immer.produce(state, draftState => {})
-  }
+  let state = immer.produce(initialState, draftState => {})
+  onStateChanged(state, saver, _options)
 
   const getState = () => {
-    return tryCloneAndFreeze(state, saver, _options)
+    return state
   }
   const setState = (changes: Partial<T>, sync = false) => {
-    if (_options.useImmer) {
-      state = immer.produce(state, draftState => {
-        _.assign(draftState, changes)
-      })
-    } else {
-      state = tryFreeze(_.assign({}, state, changes), _options)
-    }
+    state = immer.produce(state, draftState => {
+      _.assign(draftState, changes)
+    })
+    onStateChanged(state, saver, _options)
     if (_options.useVerbose) {
       log('updated ' + stateKey)
     }
@@ -177,14 +130,8 @@ export function createMidboss<T>(
     },
 
     produce: (producer: (draftState: T) => void, sync = true) => {
-      if (_options.useImmer) {
-        state = immer.produce(state, producer)
-        state = tryFreeze(state, _options)
-      } else {
-        let nextState = _.cloneDeep(state)
-        producer(nextState)
-        nextState = tryCloneAndFreeze(nextState, saver, _options)
-      }
+      state = immer.produce(state, producer)
+      onStateChanged(state, saver, _options)
       if (sync) {
         PubSub.publishSync(stateKey)
       } else {
@@ -249,5 +196,5 @@ export function useSubscription<T>(midboss: IMidboss<T>): T {
     }
   }, [])
 
-  return tryFreeze(state, midboss.getOptions())
+  return state
 }
